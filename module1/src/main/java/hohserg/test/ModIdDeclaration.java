44fbb9fd6c258726735900191bd025757097f1b0
hohserg1 <hohserg1@yandex.ru>
2024-08-07 17:31:13 +0300

    init

---
+++ module1/src/main/java/hohserg/test/ModIdDeclaration.java
@@ -0,0 +1,6 @@
+package hohserg.test;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "test1")
+public class ModIdDeclaration {}
---
+++ module2/src/main/java/hohserg/test/Main.java
@@ -0,0 +1,6 @@
+package hohserg.test;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "test2")
+public class Main {}
